e1f113c483081cb9778c68dcea4126e6d73dc6fb
Siva Tiyyagura <st954032@Broadcom.net>
2018-05-29 12:09:21 +0530

    post methos example

---
+++ welcome.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+    My name is <?php 
+        $_POST["name"]
+    ?>    
+    <br/>
+    
+    My email is <?php 
+        $_POST["email"]
+    ?>    
+</body>
+</html>
